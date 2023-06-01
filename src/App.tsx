61edba92b6723c9b7ea5cc65c9d0a2a39558a4e4
js.Kang <kcu1436@naver.com>
2023-06-01 16:04:42 +0900

    정리정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback, useEffect, useState} from 'react';
 
-const API_KEY = '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' //황인재
-// const API_KEY = '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' //강준식
+// const API_KEY = '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' //황인재
+const API_KEY = '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' //강준식
 
 function App() {
   const [text, setText] = useState<string>('response: ');
@@ -102,55 +102,7 @@ function App() {
   },[]);
 
   useEffect(() => {
-    // // async function
-    // (async () => {
-    //   // GPT API 호출
-    //   const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    //     method: 'post',
-    //     mode: 'cors',
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Authorization: `Bearer ${API_KEY}`,
-    //     },
-    //     body: JSON.stringify({
-    //       model: 'gpt-3.5-turbo', // GPT 모델. 'gpt-4'는 따로 신청해야 함
-    //       messages: [
-    //         ...SYSTEM_MESSAGE,
-    //         {role: 'user', content}, // 보낼 데이터
-    //       ],
-    //       user: 'test', // 유저 구분자
-    //       max_tokens: 3000, // 최대 제한 토큰 (글자수와는 약간 다름)
-    //       stream: true, // stream으로 요청
-    //     })
-    //   })
-
-    //   // 응답이 있으면 처리
-    //   if (response.ok && response.body) {
-    //     // const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
-    //     const reader = response.body.getReader()
-
-    //     // 스트림으로 받은 응답 처리 함수
-    //     while(true) {
-    //       const {value, done} = await reader.read();
-    //       if (done) { // 종료되었으면 break;
-    //         await reader.cancel();
-    //         break;
-    //       }
-
-    //       // 응답 처리
-    //       new TextDecoder().decode(value).split('data: ').forEach((decoded) => {
-    //         try {
-    //           // char 단위로 state에 삽입
-    //           const content = JSON.parse(decoded).choices[0].delta.content;
-    //           if (content) {
-    //             setText((prev) => prev + content)
-    //           }
-    //         } catch (e) {}
-    //       });
-    //     }
-
-    //   }
-    // })()
+    
   }, [])
 
   return (
