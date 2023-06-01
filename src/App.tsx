72990d1ef98d2abc3edc60515787c104b08fbc4a
js.Kang <kcu1436@naver.com>
2023-06-01 13:40:35 +0900

    ㄴㅇㄹ

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
-
-const API_KEY = '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'
+const API_KEY = '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' //황인재
+// const API_KEY = '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' //강준식
 
 function App() {
-  const [text, setText] = useState('response: ');
+  const [text, setText] = useState<string>('response: ');
+  const [content, setContent] = useState<string>('시그니처 암보험의 주계약에 대해 설명해줘.');
 
   /**
    * 분석 요청할 데이터
    * @type {string}
    */
-  const content = `시그니처 암보험의 주계약에 대해 설명해줘.`
+  // const content = `시그니처 암보험의 주계약에 대해 설명해줘.`
   const SYSTEM_MESSAGE = [
     {
       role: 'system',
@@ -21,7 +22,7 @@ function App() {
     {
       role: 'system',
       content: `
-    물어보는 것을 아래 json을 통해서만 답해주세요.
+    물어보는 것을 아래 내용을 통해서만 답해주세요.
     
 [
   {
@@ -39,13 +40,12 @@ function App() {
     "보험기간": "20년"
   }
 ]
-
     `
     }
   ];
 
-
-  useEffect(() => {
+  const getRes = () => {
+    setText('');
     // async function
     (async () => {
       // GPT API 호출
@@ -95,12 +95,72 @@ function App() {
 
       }
     })()
+  }
+
+  const onChange = useCallback((e: { target: { value: React.SetStateAction<string>; }; })=>{
+    setContent(e.target.value);
+  },[]);
+
+  useEffect(() => {
+    // // async function
+    // (async () => {
+    //   // GPT API 호출
+    //   const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    //     method: 'post',
+    //     mode: 'cors',
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: `Bearer ${API_KEY}`,
+    //     },
+    //     body: JSON.stringify({
+    //       model: 'gpt-3.5-turbo', // GPT 모델. 'gpt-4'는 따로 신청해야 함
+    //       messages: [
+    //         ...SYSTEM_MESSAGE,
+    //         {role: 'user', content}, // 보낼 데이터
+    //       ],
+    //       user: 'test', // 유저 구분자
+    //       max_tokens: 3000, // 최대 제한 토큰 (글자수와는 약간 다름)
+    //       stream: true, // stream으로 요청
+    //     })
+    //   })
+
+    //   // 응답이 있으면 처리
+    //   if (response.ok && response.body) {
+    //     // const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
+    //     const reader = response.body.getReader()
+
+    //     // 스트림으로 받은 응답 처리 함수
+    //     while(true) {
+    //       const {value, done} = await reader.read();
+    //       if (done) { // 종료되었으면 break;
+    //         await reader.cancel();
+    //         break;
+    //       }
+
+    //       // 응답 처리
+    //       new TextDecoder().decode(value).split('data: ').forEach((decoded) => {
+    //         try {
+    //           // char 단위로 state에 삽입
+    //           const content = JSON.parse(decoded).choices[0].delta.content;
+    //           if (content) {
+    //             setText((prev) => prev + content)
+    //           }
+    //         } catch (e) {}
+    //       });
+    //     }
+
+    //   }
+    // })()
   }, [])
 
   return (
-      <div>
+      <>
+        <div>
+          <input type="text" style={{width: '500px'}} value={content} onChange={onChange} id="keywords" name="keywords" required />
+          <button onClick={getRes}> 질문하기 </button>
+        </div>
         <div>{text}</div>
-      </div>
+      </>
   );
 }
 
