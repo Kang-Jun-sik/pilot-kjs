9eb0620bdbd71267842da702ab3e1b1d315a1268
js.Kang <kcu1436@naver.com>
2023-08-03 17:37:17 +0900

    upstage api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
-const API_KEY = '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'
+const API_KEY = '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'
 
 function App() {
   const [text, setText] = useState<string>('response: ');
@@ -11,57 +11,22 @@ function App() {
    * @type {string}
    */
   // const content = `시그니처 암보험의 주계약에 대해 설명해줘.`
-  const SYSTEM_MESSAGE = [
-    {
-      role: 'system',
-      content: `
-      당신은 데이터 분석 전문가입니다.
-      `,
-    },
-    {
-      role: 'system',
-      content: `
-    아래는 IT운영팀 구성원 정보입니다. 물어보는 것을 아래 내용을 통해서만 답해주세요.
-    
-[
-  {
-    "성명:" : "신명철",
-    "세부직무:" : "IT기획",
-  },
-  {
-    "성명:" : "김창완",
-    "세부직무:" : "서비스기획",
-  },
-  {
-    "성명:" : "강준식",
-    "세부직무:" : "프론트개발자",
-  }
-]
-    `
-    }
-  ];
-
   const getRes = () => {
     setText('');
     // async function
     (async () => {
       // GPT API 호출
-      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      const response = await fetch('https://hanwha.llmapps.apistage.ai/chat', {
         method: 'post',
         mode: 'cors',
         headers: {
+          "X-API-Key" : "zR2su0OWgM40GZkXfaqC8P94UTeX5tXaH7ObhBPd",
           "Content-Type": "application/json",
-          Authorization: `Bearer ${API_KEY}`,
+          // Authorization: `Bearer ${API_KEY}`,
         },
         body: JSON.stringify({
-          model: 'gpt-3.5-turbo', // GPT 모델. 'gpt-4'는 따로 신청해야 함
-          messages: [
-            ...SYSTEM_MESSAGE,
-            {role: 'user', content}, // 보낼 데이터
-          ],
-          user: 'test', // 유저 구분자
-          max_tokens: 3000, // 최대 제한 토큰 (글자수와는 약간 다름)
-          stream: true, // stream으로 요청
+          "conversation_id": "202307210847592230068#9323e90f-7edb-4353-a254-0bf372941c7f",
+          "query": "납입면제 사유를 알려줘"
         })
       })
 
@@ -71,45 +36,47 @@ function App() {
         const reader = response.body.getReader()
 
         // 스트림으로 받은 응답 처리 함수
-        while(true) {
-          const {value, done} = await reader.read();
+        while (true) {
+          const { value, done } = await reader.read();
           if (done) { // 종료되었으면 break;
             await reader.cancel();
             break;
           }
-
+          //debugger;
           // 응답 처리
-          new TextDecoder().decode(value).split('data: ').forEach((decoded) => {
-            try {
-              // char 단위로 state에 삽입
-              const content = JSON.parse(decoded).choices[0].delta.content;
-              if (content) {
-                setText((prev) => prev + content)
-              }
-            } catch (e) {}
-          });
+          // new TextDecoder().decode(value).split('data: ').forEach((decoded) => {
+          //   try {
+          //     // char 단위로 state에 삽입
+          //     const content = JSON.parse(decoded).choices[0].delta.content;
+          //     if (content) {
+          //       setText((prev) => prev + content)
+          //     }
+          //   } catch (e) { }
+          // });
+
+          setText((prev) => prev + new TextDecoder().decode(value));
         }
 
       }
     })()
   }
 
-  const onChange = useCallback((e: { target: { value: React.SetStateAction<string>; }; })=>{
+  const onChange = useCallback((e: { target: { value: React.SetStateAction<string>; }; }) => {
     setContent(e.target.value);
-  },[]);
+  }, []);
 
   useEffect(() => {
-    
+
   }, [])
 
   return (
-      <>
-        <div>
-          <input type="text" style={{width: '500px'}} value={content} onChange={onChange} id="keywords" name="keywords" required />
-          <button onClick={getRes}> 질문하기 </button>
-        </div>
-        <div>{text}</div>
-      </>
+    <>
+      <div>
+        <input type="text" style={{ width: '500px' }} value={content} onChange={onChange} id="keywords" name="keywords" required />
+        <button onClick={getRes}> 질문하기 </button>
+      </div>
+      <div>{text}</div>
+    </>
   );
 }
 
